fcaab2de9dc35d5a2ec612a456643b2da5fb0520
Marcus Boorstin <mindstormsguy@verizon.net>
2017-01-21 18:03:16 -0500

    It works!

--- output/output.c
+++ output/output.c
@@ -9,12 +9,15 @@
 #include "output/config.h"
 #include "output/slice.h"
 #include "output/lux.h"
+#include "output/pixel_pusher.h"
 
 static volatile int output_running;
 static volatile int output_refresh_request = false;
 static SDL_Thread * output_thread;
 static struct render * render = NULL;
 static bool output_on_lux = false;
+// TODO: Clean up the shear duplication, depending on how much there ends up being
+static bool output_on_pp = false;
 
 static int output_reload_devices() {
     // Tear down
@@ -22,8 +25,13 @@ static int output_reload_devices() {
         output_lux_term();
     }
 
+    if (output_on_pp) {
+        output_pp_term();
+    }
+
     // Reload configuration
     output_on_lux = false;
+    output_on_pp = false;
     int rc = output_config_load(&output_config, params.paths.output_config);
     if (rc < 0) {
         ERROR("Unable to load output configuration");
@@ -35,6 +43,12 @@ static int output_reload_devices() {
         int rc = output_lux_init();
         if (rc < 0) PERROR("Unable to initialize lux");
         else output_on_lux = true;
+    }
+
+    if (output_config.pixel_pusher.enabled) {
+        int rc = output_pp_init();
+        if (rc < 0) PERROR("Unable to initialize pixel pusher");
+        else output_on_pp = true;
     }
 
     return 0;
@@ -51,7 +65,7 @@ int output_run(void * args) {
     double stat_ops = 100;
     int render_count = 0;
 
-    output_running = true;   
+    output_running = true;
     int last_tick = SDL_GetTicks();
     unsigned int last_output_render_count = output_render_count;
     (void) last_output_render_count; //TODO
@@ -74,6 +88,10 @@ int output_run(void * args) {
             if (rc < 0) PERROR("Unable to sync lux frame");
         }
 
+        if (output_on_pp) {
+            if (output_pp_do_frame() < 0) PERROR("Unable to do PixelPusher frame");
+        }
+
         //SDL_framerateDelay(&fps_manager);
         SDL_Delay(1);
         int tick = SDL_GetTicks();
@@ -93,6 +111,7 @@ int output_run(void * args) {
 
     // Destroy output
     if(output_on_lux) output_lux_term();
+    if (output_on_pp) output_pp_term();
     output_config_del(&output_config);
 
     INFO("Output stopped");
